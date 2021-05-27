081cb65320c366c459859fb5c614cf3811ff4e12
iamyogesh21 <singhyogesh2104>
2021-05-27 07:49:55 +0530

    add program

--- String.c
+++ String.c
@@ -106,3 +106,29 @@ int main() {
         printf("String in lower Case = %s", str);
         return 0;
       }
+
+
+// Reverse the string 
+#include <stdio.h>
+int main()
+{
+  char str[100], rev[100];
+  int i, j, count = 0;
+  scanf("%s", str);
+  printf("String Before Reverse: %s", str);
+  //finding the length of the string
+  while (str[count] != '\0')
+  {
+    count++;
+  }
+  j = count - 1;
+
+  //reversing the string by swapping
+  for (i = 0; i < count; i++)
+  {
+    rev[i] = str[j];
+    j--;
+  }
+
+  printf("String After Reverse: %s", rev);
+}
