bc0eff9a1535fced73b5a11d14d38e85067f822d
iamyogesh21 <singhyogesh2104>
2021-05-27 07:46:16 +0530

    add program

--- String.c
+++ String.c
@@ -1,3 +1,5 @@
+// find length of string
+
 #include <stdio.h>
 int main()
 {
@@ -28,8 +30,6 @@ int main()
 * ? finally print count
 */
 
-
-
 #include <stdio.h>
 int main(){
     int i = 0,count = 0;
@@ -50,3 +50,59 @@ int main(){
 }
 
 
+// convert the string into upper case
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+   char str[100];
+   int i;
+   printf("Enter a string : ");
+   gets(str);
+   for (i = 0; str[i]!='\0'; i++) {
+      if(str[i] >= 'a' && str[i] <= 'z') {
+         str[i] = str[i] -32;
+      }
+   }
+   printf("String in Upper Case = %s", str);
+   return 0;
+
+      /**
+       * ! in this problem we use ASCII value
+       * @brief  
+       * for loop is used to convert the string into upper case string and if 
+       * block is used to check that if characters are in lower case then, 
+       * convert them in upper case by subtracting 32 from their ASCII value
+       *          If (str[i] >= 'a' and str[i] <= 'z') then 
+       *              str[i] = str[i] - 32;
+       * 
+       */
+
+}
+
+// convert the string into lower case
+      /**
+       * ! in this problem we use ASCII value
+       * @brief  
+       * for loop is used to convert the string into lower case string and if 
+       * block is used to check that if characters are in upper case then, 
+       * convert them in lower case by adding 32 from their ASCII value
+       *          If (str[i] >= 'A' and str[i] <= 'Z') then 
+       *              str[i] ← str[i] + 32;
+       * 
+       */
+      #include <stdio.h>
+      #include <string.h>
+      int main() {
+        char str[100];
+        int i;
+        printf("Enter a string : ");
+        gets(str);
+        for (i = 0; str[i]!='\0'; i++) {
+            if(str[i] >= 'A' && str[i] <= 'Z') {
+              str[i] = str[i] + 32;
+            }
+        }
+        printf("String in lower Case = %s", str);
+        return 0;
+      }
