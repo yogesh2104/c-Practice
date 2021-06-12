7845f2028456c9e5bf67927682a6ef0338e49e81
iamyogesh21 <singhyogesh2104>
2021-06-12 17:04:08 +0530

    Update Struct.c

--- Struct.c
+++ Struct.c
@@ -8,9 +8,11 @@
  * ? use the strcmp() method to compare string 
  //! syntax of strcmp == int strcmp(const char *leftStr, const char *rightStr)
 */
+
 struct emp{
     char name[20];
-    int age, salary;
+    int age;
+    float salary;
 };
 struct emp arr[100], temp;
 void main(){
@@ -20,18 +22,17 @@ void main(){
     // printf("%d",no_of_emp);
     printf("Enter emp information as name,age,salary\n");
     for(i=0;i<no_of_emp;i++){
-        // scanf("%s %d %d",&arr[i].name,&arr[i].age,&arr[i].salary);
         printf("Enter %d employee details:\n",i+1);
         printf("Name:\n");
         scanf("%s",&arr[i].name);
         printf("Age:\n");
         scanf("%d",&arr[i].age);
         printf("Salary:\n");
-        scanf("%d",&arr[i].salary);
+        scanf("%f",&arr[i].salary);
     }
     printf("\nEmp_Name\tEmp_age\t\tEmp_salary\n");
     for(i=0;i<no_of_emp;i++){
-        printf("%s\t\t%d\t\t%d\n\n",arr[i].name,arr[i].age,arr[i].salary);
+        printf("%s\t\t%d\t\t%f\n\n",arr[i].name,arr[i].age,arr[i].salary);
     }
     for(i = 1; i<no_of_emp; i++){
         for(j=0;j<no_of_emp - i ;j++){
@@ -45,7 +46,7 @@ void main(){
     printf("Employee details sort by Name:\n");
     printf("\nEmp_Name\tEmp_age\t\tEmp_salary\n");
     for(i=0;i<no_of_emp;i++){
-        printf("%s\t\t%d\t\t%d\n",arr[i].name,arr[i].age,arr[i].salary);
+        printf("%s\t\t%d\t\t%f\n",arr[i].name,arr[i].age,arr[i].salary);
     }
      for(i = 1; i<no_of_emp; i++){
         for(j=0;j<no_of_emp - i ;j++){
@@ -56,9 +57,10 @@ void main(){
             }
         }
     }
+    printf("\n");
     printf("Employee details sort by Age:\n");
     printf("\nEmp_Name\tEmp_age\t\tEmp_salary\n");
     for(i=0;i<no_of_emp;i++){
-        printf("%s\t\t%d\t\t%d\n",arr[i].name,arr[i].age,arr[i].salary);
+        printf("%s\t\t%d\t\t%f\n",arr[i].name,arr[i].age,arr[i].salary);
     }
 }
